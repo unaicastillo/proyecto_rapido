d1af73c847ec1ad9acb67a09559567f1aca17d49
unaicastillo <unaicastillocarrillo@gmail.com>
2025-03-30 21:34:42 +0200

    -Lectura de ficheros _escritura de xml y csv

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        
+        ArrayList arrayList = new ArrayList<>();
+        System.out.println("CSV");
+        arrayList=GestionFicheros.leerCsv();
+        GestionFicheros.pintarArrayList(arrayList);
+        System.out.println("XML");
+        arrayList=GestionFicheros.leerXml();
+        GestionFicheros.pintarArrayList(arrayList);
+        System.out.println("JSON");
+        arrayList=GestionFicheros.leerJson();
+        GestionFicheros.pintarArrayList(arrayList);
+
+        GestionFicheros.escribirCsv(arrayList);
+        GestionFicheros.escribirXml(arrayList);
+
+        
     }
 }
---
+++ src/GestionFicheros.java
@@ -0,0 +1,264 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+public class GestionFicheros {
+    static String  pathInput = "input/";
+    static String pathOutput = "output/";
+    public static ArrayList leerXml() throws ParserConfigurationException, SAXException, IOException{
+        File fXml = new File(pathInput+"coches.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(fXml);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("coche");
+       
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>(); 
+        HashMap<String, String> mapaCoches;
+
+        for(int i = 0 ; i < nList.getLength(); i++){
+            Node node = nList.item(i);
+            
+            if(node.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) node;
+                mapaCoches = new HashMap<>();               
+
+                
+                String marca = element.getElementsByTagName("Marca").item(0).getTextContent();
+                String modelo = element.getElementsByTagName("Modelo").item(0).getTextContent();
+                String color = element.getElementsByTagName("Color").item(0).getTextContent();
+
+                String precio = element.getElementsByTagName("Precio").item(0).getTextContent();
+                
+                mapaCoches.put("Marca", marca);
+                mapaCoches.put("Modelo", modelo);
+                mapaCoches.put("Color", color);
+                mapaCoches.put("Precio", precio);
+
+                arrayList.add(mapaCoches); 
+
+                
+            }
+
+            
+
+
+        } 
+        return arrayList;
+        
+    } 
+
+
+    public static ArrayList leerCsv(){
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        String archivoCsv = pathInput+"coches.csv"; 
+        HashMap<String, String> mapaCoches ;
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoCsv))) {
+            String line;
+            String lineReader=br.readLine();
+            String[] encabezados = lineReader.split(","); 
+
+            while ((line = br.readLine()) != null) {
+                mapaCoches = new HashMap<>();
+                String[] valores = line.split(",");
+                
+                
+                for (int i = 0; i < encabezados.length; i++) {
+                    mapaCoches.put(encabezados[i], valores[i]); 
+                }
+                
+                
+                arrayList.add(mapaCoches);
+                
+            }
+
+            
+
+
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo CSV: " + e.getMessage());
+        }
+        return arrayList;
+
+    }
+
+
+
+
+    public static ArrayList leerJson(){
+        JSONParser parser = new JSONParser();
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+        HashMap<String, String> mapaCoches;
+        try {
+            JSONArray arrayJson = (JSONArray) parser.parse(new FileReader("input/coches.json"));
+
+            for (int i = 0; i < arrayJson.size(); i++) {
+                mapaCoches = new HashMap<>();
+                JSONObject jsonObject = (JSONObject) arrayJson.get(i);
+                
+                String marca = (String) jsonObject.get("Marca");
+                String modelo = (String) jsonObject.get("Modelo");
+                String color = (String) jsonObject.get("Color");
+                Long anio = (Long) jsonObject.get("Año");
+                Double precio = (Double) jsonObject.get("Precio");
+                mapaCoches.put("Marca", marca);
+                mapaCoches.put("Modelo", modelo);
+                mapaCoches.put("Color", color);
+                mapaCoches.put("Año", ""+anio);
+                mapaCoches.put("Precio", ""+precio);
+                arrayList.add(mapaCoches);
+
+            }
+            
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return arrayList;
+
+    }
+
+    
+
+    public static void  pintarArrayList(ArrayList arrayList){
+        for (int i = 0; i < arrayList.size(); i++) {
+            HashMap<String, String> mapaCoches = (HashMap<String, String>) arrayList.get(i);
+            System.out.println("Marca: " + mapaCoches.get("Marca") + ", Modelo: " + mapaCoches.get("Modelo")+ ", Año: "+ mapaCoches.get("Año") + ", Color: " + mapaCoches.get("Color") + 
+            ", Precio: " + mapaCoches.get("Precio"));
+        }
+    }
+
+
+    public static void escribirCsv(ArrayList<HashMap<String, String>> lista) {
+        String archivoCsv = pathOutput+"coches.csv";
+        
+        
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(archivoCsv, "UTF-8");
+            
+            
+            writer.println("Marca,Modelo,Año,Color,Precio");
+            
+            for (int i = 0 ; i < lista.size(); i++) {
+                HashMap<String, String> mapaCoches = lista.get(i);
+                writer.println(mapaCoches.get("Marca") + "," + mapaCoches.get("Modelo") + "," + mapaCoches.get("Año") + "," + mapaCoches.get("Color") + "," + mapaCoches.get("Precio"));
+            }
+            
+            writer.close();
+            
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }   
+    }
+    public static void escribirXml(ArrayList<HashMap<String, String>> lista) {
+        String archivoXml = pathOutput+"coches.Xml";
+
+
+       
+        try {
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.newDocument();
+            FileOutputStream output = new FileOutputStream(archivoXml);
+            Element rootElement, cocheElement, marcaCoche, modeloCoche, anioCoche, colorCoche, precioCoche;
+            rootElement = doc.createElement("coches");
+
+            HashMap<String, String> mapaCoches ;
+            doc.appendChild(rootElement);
+            
+
+            for (int i = 0; i < lista.size(); i++) {
+                mapaCoches = lista.get(i);
+                cocheElement = doc.createElement("coche");
+
+                rootElement.appendChild(cocheElement);
+                
+                marcaCoche= doc.createElement("Marca");
+                marcaCoche.appendChild(doc.createTextNode(mapaCoches.get("Marca")));
+                cocheElement.appendChild(marcaCoche);
+
+                modeloCoche= doc.createElement("Modelo");
+                modeloCoche.appendChild(doc.createTextNode(mapaCoches.get("Modelo")));
+                cocheElement.appendChild(modeloCoche);
+
+                anioCoche= doc.createElement("Año");
+                anioCoche.appendChild(doc.createTextNode(mapaCoches.get("Año")));
+                cocheElement.appendChild(anioCoche);
+
+                colorCoche= doc.createElement("Color");
+                colorCoche.appendChild(doc.createTextNode(mapaCoches.get("Color")));
+                cocheElement.appendChild(colorCoche);
+
+                precioCoche= doc.createElement("Precio");
+                precioCoche.appendChild(doc.createTextNode(mapaCoches.get("Precio")));
+                cocheElement.appendChild(precioCoche);
+            }
+
+
+            
+            
+
+
+
+
+
+            saveXml(doc, output);
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+
+    private static void saveXml(Document doc, OutputStream filePath) {
+        try {
+            
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(filePath);
+
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
